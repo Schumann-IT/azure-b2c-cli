77bb3fc0a1b34d1bb6b00827b97c5acec4a752ac
Jan Schumann <js@schumann-it.com>
2024-04-25 10:22:37 +0200

    replace debug flag with DEBUG env var

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+
 	"com.schumann-it.azure-b2c-cli/cmd/application"
 	"com.schumann-it.azure-b2c-cli/cmd/policy"
 	"com.schumann-it.azure-b2c-cli/cmd/serviceprincipal"
@@ -15,8 +17,7 @@ var rootCmd = &cobra.Command{
 	Short: "Tooling for Azure B2C",
 	PersistentPreRun: func(cmd *cobra.Command, _ []string) {
 		lvl := logging.INFO
-		isDebug, _ := cmd.PersistentFlags().GetBool("debug")
-		if isDebug {
+		if os.Getenv("DEBUG") == "true" {
 			lvl = logging.DEBUG
 		}
 		logging.SetLevel(lvl, "")
@@ -28,7 +29,6 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.PersistentFlags().Bool("debug", false, "Enable debug mode")
 	rootCmd.AddCommand(policy.RootCmd)
 	rootCmd.AddCommand(application.RootCmd)
 	rootCmd.AddCommand(serviceprincipal.RootCmd)
